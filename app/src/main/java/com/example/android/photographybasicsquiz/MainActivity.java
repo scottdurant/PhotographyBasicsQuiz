2b95aa8f4d23f9f4e7e41f4d96e53b67032a08ea
Scott Durant <scott.durant.16@cnu.edu>
2017-09-05 19:54:01 -0400

    added a new activity to display score

--- app/src/main/java/com/example/android/photographybasicsquiz/MainActivity.java
+++ app/src/main/java/com/example/android/photographybasicsquiz/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.android.photographybasicsquiz;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -74,7 +75,17 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-        Toast.makeText(this, "You scored " + score + "/5", Toast.LENGTH_LONG).show();
+        Button myButton = (Button) findViewById(R.id.calculateScoreButton);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent scoreIntent = new Intent(MainActivity.this, score.class);
+                startActivity(scoreIntent);
+
+            }
+        });
+
+        //Toast.makeText(this, "You scored " + score + "/5", Toast.LENGTH_LONG).show();
 
     }
 }
